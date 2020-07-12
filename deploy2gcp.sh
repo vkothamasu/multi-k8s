ec14dc49b32cf848090bd1595be46c249a4f4687
Kothamasu <vasudeva.kothamasu@gmail.com>
2020-07-12 14:16:42 +0530

    travis yaml test 5

--- deploy2gcp.sh
+++ deploy2gcp.sh
@@ -1,4 +1,4 @@
-docker build -t kothamasu/multi-client:latest -t kothamasu/multi-client:$GIT_SHA -f ./client/Dockerfile:latest ./client
+docker build -t kothamasu/multi-client:latest -t kothamasu/multi-client:$GIT_SHA -f ./client/Dockerfile ./client
 docker build -t kothamasu/multi-server:latest -t kothamasu/multi-server:$GIT_SHA -f ./server/Dockerfile ./server
 docker build -t kothamasu/multi-worker:latest -t kothamasu/multi-worker:$GIT_SHA -f ./worker/Dockerfile ./worker
 
