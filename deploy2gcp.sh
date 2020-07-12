7d2aae7cb0ac7ac60a09459882c2a4e57dd202d9
Kothamasu <vasudeva.kothamasu@gmail.com>
2020-07-12 14:52:23 +0530

    dploy command updated

--- deploy2gcp.sh
+++ deploy2gcp.sh
@@ -10,4 +10,8 @@ docker push kothamasu/multi-client:latest
 docker push kothamasu/multi-worker:latest
 docker push kothamasu/multi-server:latest
 
-kubectl delete -f k8s
+kubectl apply -f k8s
+
+kubectl set image deployments/server-deployment server=kothamasu/multi-server:$GIT_SHA
+kubectl set image deployments/client-deployment server=kothamasu/multi-client:$GIT_SHA
+kubectl set image deployments/worker-deployment server=kothamasu/multi-worker:$GIT_SHA
